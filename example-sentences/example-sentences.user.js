736c7351b6d5485d056e54095356bab539a8633b
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2017-10-25 10:41:42 +1100

    everything works, but haven't tested reviews yet

--- example-sentences/example-sentences.user.js
+++ example-sentences/example-sentences.user.js
@@ -2,10 +2,11 @@
 // @name WaniKani Example Sentences
 // @version 2.1
 // @description  Displays additional examples sentences for the given vocabulary.
+// @require https://raw.github.com/jeshuam/wanikani-userscripts/master/utility/api.js
 // @match https://www.wanikani.com/settings/account
-// @match *://www.wanikani.com/*vocabulary/*
-// @match *://www.wanikani.com/review/session*
-// @match *://www.wanikani.com/lesson/session*
+// @match https://www.wanikani.com/vocabulary/*
+// @match https://www.wanikani.com/review/session*
+// @match https://www.wanikani.com/lesson/session*
 // @run-at          document-end
 // @copyright 2017 jeshuam
 // @grant       GM_getValue
@@ -14,7 +15,53 @@
 // @grant       GM_addStyle
 // ==/UserScript==
 
-function DisplayExampleSentences(LEARNED_VOCABULARY) {
+//////////////////////////
+/////// CSS Styles ///////
+//////////////////////////
+GM_addStyle(`
+div#example-sentences {
+max-height: 400px;
+overflow: auto;
+}
+
+.example-sentence-unlearned-vocab {
+display: none;
+}
+
+#example-sentences-toggle-display {
+margin-bottom: 20px;
+}`);
+
+
+/////////////////////////
+/////// Constants ///////
+/////////////////////////
+// The amount of time before the learned item cache is updated.
+let CACHE_EXPIRATION_TIMER_MS = 24 * 60 * 60 * 1000; // (1 day)
+
+
+/////////////////////////////////
+/////// Utility Functions ///////
+/////////////////////////////////
+//
+// Determine whether the given Japanese word ends in an 'u' sound (i.e. is a verb).
+//
+function EndsInUSound(japaneseWord) {
+  return japaneseWord.match(/[るゆむふぬつすくう]/) !== null;
+}
+
+//
+// Function to check whether a Japanese word only contains kana (hira- or katakana).
+//
+function OnlyContainsKanaOrPunctuation(japaneseWord) {
+  return japaneseWord.match(/^[\u3000-\u30FF]+$/) !== null;
+}
+
+
+/////////////////////////////
+/////// Main Function ///////
+/////////////////////////////
+function DisplayExampleSentences(known_vocab) {
   //
   // Extract the Kanji from the current page. This will have a switch for each type of page, and
   // will do something different for each. This will only include vocabulary pages, reviews and
@@ -43,33 +90,6 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
   }
 
   //
-  // Extract the kana from the current page. This will have a switch for each type of page, and
-  // will do something different for each. This will only include vocabulary pages, reviews and
-  // lessons.
-  //
-  function GetVocabularyKanaFromPage() {
-    // Vocabulary information page.
-    if (document.URL.indexOf('vocabulary') != -1) {
-      return $('section.vocabulary-reading p').text().trim();
-    }
-
-    // Review page.
-    else if (document.URL.indexOf('review/session') != -1) {
-      return $.jStorage.get('currentItem').kana[0];
-    }
-
-    // Lesson page.
-    else if (document.URL.indexOf('lesson/session') != -1) {
-      return $.jStorage.get('l/currentLesson').kana[0];
-    }
-
-    // Not on a valid page.
-    else {
-      return null;
-    }
-  }
-
-  //
   // Get the data from the remote URL for the given vocabulary.
   //
   function GetExampleSentencesForVocabulary(vocabulary, complete) {
@@ -77,14 +97,11 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
   }
 
   //
-  // Function to check whether a Japanese word only contains kana (hira- or katakana).
+  // Generate the DOM required to display the example sentences. This will be consistent over the
+  // various pages this script runs on.
   //
-  function OnlyContainsKanaOrPunctuation(japaneseWord) {
-    return japaneseWord.match(/^[\u3000-\u30FF]+$/) !== null;
-  }
-
   function GetSectionWithExamplesSentences(known_vocab, sentences) {
-    // Make something that looks like this:
+    // Make the initial section.
     let section = document.createElement('section');
     section.id = 'examples-sentences-section';
     section.innerHTML = `
@@ -93,6 +110,7 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
 <div id="example-sentences"></div>
 `;
 
+    // When the button is pressed, show/hide the example sentences with unlearned vocab.
     section.children[1].onclick = function() {
       if (this.innerText === 'Show All Sentences') {
         for (let element of document.querySelectorAll('.example-sentence-unlearned-vocab')) {
@@ -109,11 +127,11 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
       }
     };
 
+    // Add each sentence to the section.
     for (let sentence of sentences) {
-      // Check if this sentence has any words we don't know.
+      // Check if this sentence has any words we don't know. If it does, add an extra class to it.
       let extra_class = '';
       let kanji = GetVocabularyKanjiFromPage();
-      let kana = GetVocabularyKanaFromPage();
       for (let word of sentence.jpn) {
         if (word != kanji && !OnlyContainsKanaOrPunctuation(word) && known_vocab[word] === undefined) {
           extra_class = 'example-sentence-unlearned-vocab';
@@ -122,11 +140,11 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
       }
 
       // Make the HTML for this sentence.
-      var japanese_html = '';
+      let japanese_html = '';
       for (let word of sentence.jpn) {
         // Highlight the current word.
         if (word == kanji) {
-          japanese_html += '<span class="vocabulary-highlight highlight-vocabulary" rel="tooltip" data-original-title="' + kana + '">' + kanji + '</span>';
+          japanese_html += '<span class="vocabulary-highlight highlight-vocabulary">' + kanji + '</span>';
         }
 
         // Insert a link to the WaniKani page for learned vocabulary.
@@ -141,17 +159,10 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
       }
 
       let sentence_html = document.createElement('div');
-      sentence_html.className = 'context-sentence-group ' + extra_class;
-      sentence_html.innerHTML = `
-<p lang="ja">${japanese_html}</p>
-<p>${sentence.eng}</p>
-`;
+      sentence_html.className = `context-sentence-group ${extra_class}`;
+      sentence_html.innerHTML = `<p lang="ja">${japanese_html}</p><p>${sentence.eng}</p>`;
 
       section.children[2].appendChild(sentence_html);
-    }
-
-    if (section.children[2].children.length === 0) {
-      section.children[2].innerHTML = 'No example sentences found for this word!';
     }
 
     return section;
@@ -159,25 +170,25 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
 
   // Process the vocabulary page.
   if (document.URL.indexOf('vocabulary') >= 0) {
-    var vocabulary = $('header span.vocabulary-icon span').text();
+    let vocabulary = document.querySelector('header span.vocabulary-icon span').innerText;
 
     GetExampleSentencesForVocabulary(vocabulary, function(data) {
       if (data.length === 0) {
         return;
       }
 
-      let section = GetSectionWithExamplesSentences(LEARNED_VOCABULARY, data);
+      let section = GetSectionWithExamplesSentences(known_vocab, data);
       let insertion_section = document.querySelector('section.context-sentence');
       insertion_section.parentNode.insertBefore(section, insertion_section.nextSibling);
     });
   }
 
-  // Process the review page.
+  // Process the review page. TODO(jeshua): test this.
   else if (document.URL.indexOf('review/session') >= 0) {
     // Display the information when the current item changes. Don't do this if they are
     // supposed to enter the reading.
     $.jStorage.listenKeyChange('currentItem', function(key) {
-      var currentVocab = $.jStorage.get(key).voc;
+      let currentVocab = $.jStorage.get(key).voc;
       GetExampleSentencesForVocabulary(currentVocab, function(data) {
         // Make sure the current vocabulary still matches what is on the page (this
         // is to prevent multiple sentence boxes from showing up).
@@ -185,41 +196,41 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
           return;
         }
 
-        if (data.length == 0) {
+        if (data.length === 0) {
           return;
         }
 
-        if ($('#answer-form input').attr('lang') != 'ja') {
-          var section = GetSectionWithExamplesSentences(data);
-          $('div#all-info').before(section.prepend('<br />'));
-          SetupPaginationOnSectionWithSentenceData(section, data);
+        if (document.querySelector('#answer-form input').attributes.lang != 'ja') {
+          let section = GetSectionWithExamplesSentences(known_vocab, data);
+          let insertion_section = document.querySelector('div#all-info');
+          insertion_section.parentNode.insertBefore(section, insertion_section.nextSibling);
         }
       });
     });
 
     // If the 'all-info' button is pressed, then display it.
-    $('div#all-info').on('click', function() {
+    document.querySelector('div#all-info').onclick = function() {
       GetExampleSentencesForVocabulary($.jStorage.get('currentItem').voc, function(data) {
-        if (data.length == 0) {
+        if (data.length === 0) {
           return;
         }
 
-        var section = GetSectionWithExamplesSentences(data);
-        $('div#all-info').before(section.prepend('<br />'));
-        SetupPaginationOnSectionWithSentenceData(section, data);
+        let section = GetSectionWithExamplesSentences(known_vocab, data);
+        let insertion_section = document.querySelector('div#all-info');
+        insertion_section.parentNode.insertBefore(section, insertion_section.nextSibling);
       });
-    });
+    };
   }
 
   // Process the lesson page.
   else if (document.URL.indexOf('lesson/session') >= 0) {
     $.jStorage.listenKeyChange('l/currentLesson', function(key) {
       GetExampleSentencesForVocabulary($.jStorage.get(key).voc, function(data) {
-        if (data.length == 0) {
+        if (data.length === 0) {
           return;
         }
 
-        var section = GetSectionWithExamplesSentences(LEARNED_VOCABULARY, data);
+        let section = GetSectionWithExamplesSentences(known_vocab, data);
         let insertion_section = document.querySelector('div#supplement-voc-context-sentence');
         insertion_section.parentNode.insertBefore(section, insertion_section.nextSibling);
       });
@@ -227,32 +238,29 @@ function DisplayExampleSentences(LEARNED_VOCABULARY) {
   }
 }
 
-function EndsInUSound(japaneseWord) {
-  return japaneseWord.match(/[るゆむふぬつすくう]/) !== null;
-}
 
-function main() {
+//////////////////////////////
+/////// Start Function ///////
+//////////////////////////////
+window.onload = function() {
   // Get their API key. If we are on the account page, go no further.
   let API_KEY = WaniKaniAPI.getAPIKey();
   if (window.location.href.indexOf('account') >= 0) {
     return;
   }
 
-  // Default expiration timer to 1 day (24 hours, 60 minutes, 60 seconds, 1000 ms).
-  var CACHE_EXPIRATION_TIMER_MS = 24 * 60 * 60 * 1000;
-
   // Load the unlocked vocab, initializing to an empty object.
-  var unlockedVocab = JSON.parse(GM_getValue('wanikani-sentences-learned-cache', '{"__cache-time": 0}'));
-  var currentTime = (new Date().getTime());
+  let unlockedVocab = JSON.parse(GM_getValue('wanikani-sentences-learned-cache', '{"__cache-time": 0}'));
 
   // If cache expired, update first then run main.
+  let currentTime = (new Date().getTime());
   if ((currentTime - unlockedVocab['__cache-time']) > CACHE_EXPIRATION_TIMER_MS) {
     console.log('EXAMPLE-SENTENCES: Cache expired, refreshing known vocabulary.');
     WaniKaniAPI.load(WaniKaniAPI.apiURL('vocabulary'), function(data) {
       for (let vocab of data.requested_information.general) {
         if (vocab.user_specific !== null) {
           // Remove any preceding ~ characters (as they aren't part of the word).
-          var kanji = vocab.character.replace(/〜/g, '');
+          let kanji = vocab.character.replace(/〜/g, '');
           unlockedVocab[kanji] = kanji;
 
           // For verbs, remove the U sound at the end. This should do a decent job of showing
@@ -273,22 +281,4 @@ function main() {
   } else {
     DisplayExampleSentences(unlockedVocab);
   }
-}
-
-// Add style.
-GM_addStyle(`
-div#example-sentences {
-max-height: 400px;
-overflow: auto;
-}
-
-.example-sentence-unlearned-vocab {
-display: none;
-}
-
-#example-sentences-toggle-display {
-margin-bottom: 20px;
-}
-`);
-
-window.onload = main;
+};
